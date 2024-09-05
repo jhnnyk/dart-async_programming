714953f1916f9ff187a579c70561e806e97d3877
John Knotts <jhnnyk@gmail.com>
2024-09-05 11:16:45 -0600

    Future.error and Future.value

--- futures.dart
+++ futures.dart
@@ -1,15 +1,21 @@
 Future<String> fetchUserOrder() => Future.delayed(
       Duration(seconds: 2),
-      () => 'cappuccino',
+      () => 'Cappuccino',
     );
+
+Future<String> fetchUserOrder2() => Future.value('Espresso');
+
+Future<String> fetchUserOrder3() => Future.error('Out of milk');
 
 Future<void> main() async {
   print('program started');
   try {
     final order = await fetchUserOrder();
     print(order);
-    final order2 = await fetchUserOrder();
+    final order2 = await fetchUserOrder2();
     print(order2);
+    final order3 = await fetchUserOrder3();
+    print(order3);
   } catch (e) {
     print(e);
   } finally {
