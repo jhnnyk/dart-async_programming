da3a49a9111ba7985041ce803de451db0d649de4
John Knotts <jhnnyk@gmail.com>
2024-09-05 12:37:36 -0600

    stream generators: async* and yield

--- streams.dart
+++ streams.dart
@@ -1,3 +1,5 @@
+import 'countdown.dart';
+
 Future<int> sumStream(Stream<int> stream) async {
   var sum = 0;
   await for (var value in stream) {
@@ -9,8 +11,17 @@ Future<int> sumStream(Stream<int> stream) async {
 Future<int> sumStream2(Stream<int> stream) =>
     stream.reduce((previous, element) => previous + element);
 
+Stream<int> countStream(int n) async* {
+  for (var i = 1; i <= n; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    print(i);
+    yield i;
+  }
+}
+
 Future<void> main() async {
-  final stream = Stream.fromIterable([1, 2, 3, 4]);
-  final sum = await sumStream2(stream);
+  // final stream = Stream.fromIterable([1, 2, 3, 4]);
+  final stream2 = countStream(4);
+  final sum = await sumStream2(stream2);
   print('Sum: $sum');
 }
